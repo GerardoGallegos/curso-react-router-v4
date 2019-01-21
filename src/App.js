562b925f0c049dfed93111b56b5580b52f04caab
Gerardo Gallegos <puxerop@gmail.com>
2019-01-21 13:44:24 -0600

    Leccion # 5 - Usando el componente <Link />

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const Hola = () => (
   <h2>Hola</h2>
@@ -9,23 +9,41 @@ const Productos = () => (
   <h2>productos</h2>
 )
 
-const Home = () => (
-  <h2>Home</h2>
+const Home = (props) => {
+  console.log(props)
+  return (
+    <h2>Home { props.location.state.message }</h2>
+  )
+}
+
+const navStyles = {
+  display: 'flex',
+  justifyContent: 'space-around'
+}
+
+const Navegacion = () => (
+  <nav style={navStyles}>
+    <Link to={{
+      pathname: '/',
+      search: '?ordenar=nombre',
+      hash: '#hash-otro',
+      state: {
+        message: 'Soy Ninja PRO',
+        cantidad: 555
+      }
+    }}>Home</Link>
+    <Link to='/hola'>Hola</Link>
+    <Link to='/productos'>Productos</Link>
+  </nav>
 )
 
 const App = () => (
   <BrowserRouter>
     <>
+      <Navegacion />
       <Route path='/' exact component={Home} />
       <Route path='/hola/' render={Hola} />
-      <Route path='/productos'>
-        {({ match }) => {
-          if (!match) return null
-          return (
-            <Productos />
-          )
-        }}
-      </Route>
+      <Route path='/productos' render={Productos} />
     </>
   </BrowserRouter>
 )
