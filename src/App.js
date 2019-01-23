16af8930411fe974654bdfb924e8d2f9cc317ce0
Gerardo Gallegos <puxerop@gmail.com>
2019-01-23 14:25:02 -0600

    Leccion # 13 - Ruta raiz dinamica y pagina 404 con componente <Switch />

--- src/App.js
+++ src/App.js
@@ -9,17 +9,22 @@ const Videos = () => (
   <h2>Videos</h2>
 )
 
-const Playlist = () => (
-  <h2>Playlist</h2>
+const GeneroMusical = ({ match }) => (
+  <h2>Genero: { match.params.genero }</h2>
+)
+
+const Error404 = () => (
+  <h2>Pagina no encontrada!</h2>
 )
 
 const App = () => (
   <BrowserRouter>
     <>
       <Switch>
-        <Route path='/videos' render={Playlist} />
+        <Route path='/' exact render={Home} />
         <Route path='/videos' render={Videos} />
-        <Route path='/' render={Home} />
+        <Route path='/:genero' exact render={GeneroMusical}/>
+        <Route render={Error404} />
       </Switch>
     </>
   </BrowserRouter>
