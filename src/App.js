a531c936ef9683d5efb2ccf83c6c72981a4e91ba
Gerardo Gallegos <puxerop@gmail.com>
2019-01-23 12:07:40 -0600

    Leccion # 12 - Entendiendo el componente <Switch />

--- src/App.js
+++ src/App.js
@@ -1,78 +1,26 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const Home = () => (
-  <h2>Pagina Principal</h2>
+  <h2>Home</h2>
 )
 
-class Usuarios extends Component {
-  state = {
-    users: []
-  }
+const Videos = () => (
+  <h2>Videos</h2>
+)
 
-  componentDidMount () {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }))
-  }
-
-  render () {
-    const { users } = this.state
-    return (
-      <div>
-        <h2>Usuarios</h2>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              <Link to={{
-                pathname: `/usuarios/${user.id}`,
-                state: {
-                  user
-                }
-              }}>
-                { user.name }
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
-}
-
-
-class UsuariosDetalle extends Component {
-  state = {
-    user: {}
-  }
-  componentDidMount () {
-    const { id } = this.props.match.params
-    
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(user => this.setState({ user }))
-  }
-
-  render () {
-    const { user } = this.state
-    return (
-      <div>
-        <h2>Detalles de usuario</h2>
-        <h3>{ user.name }</h3>
-        <div>Email: { user.email }</div>
-        <div>Sitio Web: { user.website } </div>
-        <div>Telefono: { user.phone } </div>
-      </div>
-    )
-  }
-}
+const Playlist = () => (
+  <h2>Playlist</h2>
+)
 
 const App = () => (
   <BrowserRouter>
     <>
-      <Route path='/' exact render={Home} />
-      <Route path='/usuarios' exact component={Usuarios}/>
-      <Route path='/usuarios/:id' component={UsuariosDetalle}/>
+      <Switch>
+        <Route path='/videos' render={Playlist} />
+        <Route path='/videos' render={Videos} />
+        <Route path='/' render={Home} />
+      </Switch>
     </>
   </BrowserRouter>
 )
