cd5cb7f3f2b8dc0e4b76017836385961eedadf20
Gerardo Gallegos <puxerop@gmail.com>
2019-01-24 15:20:24 -0600

    Leccion # 15 - Usando el component <Redirect />

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Route, NavLink, Redirect } from 'react-router-dom'
+import './App.css'
 
-const Pagina = (props) => {
-  console.log(props)
+const Navegation = () => (
+  <nav>
+    <NavLink to='/' exact activeClassName='activa'>Home</NavLink>
+    <NavLink to='/perfil' activeClassName='activa'>Perfil</NavLink>
+    <NavLink to='/login' activeClassName='activa'>Login</NavLink>
+  </nav>
+)
+
+const Home = () => (
+  <h2>Home</h2>
+)
+
+
+const Login = ({ location }) => {
+  
+  if (location.state) {
+    return <h2>{ location.state.message }</h2>
+  }
+  
   return (
-    <h2>Pagina</h2>
+    <h2>Login</h2>
   )
 }
 
-class App extends Component {
-  state = {
-    user: {
-      name: 'Gerardo Ninja',
-      country: 'Mexico'
-    }
-  }
-  render () {
-    return (
-      <BrowserRouter>
-        <Route
-          path='/:page'
-          render={(props) => (
-            <Pagina
-              {...props}
-              user={this.state.user}
-            />
-          )}
-        />
-      </BrowserRouter>
-    )
-  }
+const isAuth = false
+
+const Perfil = () => {
+  return isAuth
+    ? <h2>Bienvenido a tu Perfil</h2>
+    : <Redirect
+        to={{
+          pathname: '/login',
+          state: {
+            message: 'Debes de hacer login para acceder a tu perfil'
+          }
+        }}
+      />
 }
+
+const App = () => (
+  <BrowserRouter>
+    <>
+      <Navegation />
+      <Route path='/' exact render={Home} />
+      <Route path='/perfil' render={Perfil} />
+      <Route path='/login' render={Login} />
+      <Redirect from='/p' to='/perfil' />
+    </>
+  </BrowserRouter>
+)
 
 export default App
