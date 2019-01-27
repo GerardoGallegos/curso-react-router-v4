85bb4d2935871d3c01c3895d5aa47521e6a9cd77
Gerardo Gallegos <puxerop@gmail.com>
2019-01-26 19:51:22 -0600

    Leccion # 20 - agregando datos de rutas con withRouter

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
 import React from 'react'
-import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  withRouter
+} from 'react-router-dom'
 import './App.css'
 
-const Navegation = () => (
-  <nav>
-    <NavLink to='/' exact activeClassName='activa'>Home</NavLink>
-    <NavLink to='/ninja' activeClassName='activa'>Ninja</NavLink>
-    <NavLink to='/videos' activeClassName='activa'>Videos</NavLink>
-  </nav>
-)
+const Navegation = withRouter((props) => {
+  return (
+    <nav>
+      <NavLink to='/' exact activeClassName='activa'>Home</NavLink>
+      <NavLink to='/ninja' activeClassName='activa'>Ninja</NavLink>
+      <NavLink to='/videos' activeClassName='activa'>Videos</NavLink>
+      <button
+        onClick={props.history.goBack}
+      >Back</button>
+    </nav>
+  )
+})
 
-const Home = () => (
-  <section className='route pink'>
-    <h1>Home</h1>
-  </section>
-)
+const Home = () => <h1>Home</h1>
 
-const Ninja =  () => (
-  <section className='route blue'>
-    <h1>Ninja</h1>
-  </section>
-)
+const Ninja =  () => <h1>Ninja</h1>
 
-const Videos = () => (
-  <section className='route green'>
-    <h1>Videos</h1>
-  </section>
-)
+const Videos = () => <h1>Videos</h1>
 
 
 const App = () => (
   <BrowserRouter>
     <>
     <Navegation />
-    <Route render={({ location }) => (
-      <div className='content'>
-        <TransitionGroup>
-          <CSSTransition
-            timeout={700}
-            classNames='fade'
-            key={location.pathname}
-          >
-            <Switch location={location}>
-              <Route path='/' exact render={Home} />
-              <Route path='/ninja' component={Ninja} />
-              <Route path='/videos' render={Videos} />
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
-      </div>
-    )} />
+    <Route path='/' exact render={Home} />
+    <Route path='/ninja' component={Ninja} />
+    <Route path='/videos' render={Videos} />
     </>
   </BrowserRouter>
 )
